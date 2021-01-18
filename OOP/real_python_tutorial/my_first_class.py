bd4d877b333cc84ffc452d62e652063070f505de
bohdanmykhayliv <bohdan.mykhailiv@ucu.edu.ua>
2021-01-18 16:09:49 +0200

    Rename folder \'classes\' to  \'OOP\'

---
+++ OOP/real_python_tutorial/my_first_class.py
@@ -0,0 +1,24 @@
+class Dog:
+    # class attributes
+    species = 'Canis familiaris'
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # Instance Method
+    def __str__(self):
+        return f'{self.name} is {self.age} years old'
+
+    # Another instance method
+    def speak(self, sound):
+        return f'{self.name} says {sound}'
+
+
+lord = Dog('Lord', 5)
+jack = Dog('Jack', 11)
+
+print(jack)
+print()
+
+print(lord.speak('Wow'))
